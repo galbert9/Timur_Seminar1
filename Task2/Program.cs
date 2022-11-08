735d88689164373331d0c4c5ebdf5729193ff752
Gennady <galbert9@gmail.com>
2022-11-09 00:09:05 +0300

    Сравнение 2- чисел

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Сравнить два числа и найти большее 
+Console.WriteLine("Введите первое число");  
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите второе число"); 
+int b = Convert.ToInt32 (Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine("Max " +a);
+}
+else
+{
+    Console.WriteLine("Max " +b);
+}
