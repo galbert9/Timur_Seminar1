4ef0105d53f7b5f623468742b4b263b4ad65020c
Gennady <galbert9@gmail.com>
2022-11-09 10:48:33 +0300

    Сравнить три числа

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Сравнить три числа и выбрать наибольшее
+Console.WriteLine("Введите первое число");  
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите второе число"); 
+int b = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите третье число"); 
+int c = Convert.ToInt32 (Console.ReadLine());
+
+if (a > b & a > c) Console.WriteLine("Max " +a);
+else if (c > b) Console.WriteLine("Max " +c);
+else Console.WriteLine("Max " +b);
+
+
+    
+
