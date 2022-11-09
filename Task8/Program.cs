b6c6e203f06091b894147dfdcb854fe81fe02cde
Gennady <galbert9@gmail.com>
2022-11-09 12:47:34 +0300

    Все четные числа от нуля до N

---
+++ Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выбрать четные числа
+Console.WriteLine("Ввести число");
+int x = Convert.ToInt32 (Console.ReadLine());
+int i = -x;
+while (i <= x)
+{
+   if (i > 0 & i % 2 == 0)
+   Console.Write(i + " ");
+   i = i + 1; 
+}
+   
+
+
+
